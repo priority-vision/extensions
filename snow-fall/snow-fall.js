6df38bbde20321045a2dd39b93d41dd0865e4836
Nikita <nkdevinfo@gmail.com>
2024-12-31 01:08:06 +0300

    check if we can import styles in module (not sure it is possible)

--- snow-fall/snow-fall.js
+++ snow-fall/snow-fall.js
@@ -1,24 +1,5 @@
 // snow-effects.js
 import 'https://cdn.jsdelivr.net/npm/@zachleat/snow-fall@1.0.3/snow-fall.min.js';
+import styles from './snow-fall.css' assert { type: 'css' };
 
-// Add styles with dark mode support.
-const styles = document.createElement('style');
-styles.textContent = `
-    snow-fall {
-        z-index: 1;
-    }
-
-    :root {
-        --snow-fall-color: rgba(255, 255, 255, 1);
-    }
-
-    [data-color-scheme="dark"]:root {
-        --snow-fall-color: rgba(255, 255, 255, .2);
-    }
-    @media (prefers-color-scheme: dark) {
-        [data-color-scheme="system"]:root {
-            --snow-fall-color: rgba(255, 255, 255, .2);
-        }
-    }
-`;
-document.head.appendChild(styles);
+document.adoptedStyleSheets = [...document.adoptedStyleSheets, styles];
